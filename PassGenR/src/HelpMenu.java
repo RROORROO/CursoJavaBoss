564743b77225c7119a3913653b275c27ae3ec995
rodrigor <rodrigor@QA-RodrigoR2.visionsoftware.loc>
2018-06-08 10:17:40 -0500

    Proyecto password

---
+++ PassGenR/src/HelpMenu.java
@@ -0,0 +1,26 @@
+
+public class HelpMenu {
+	
+	public void menu (String Pass) {
+		System.out.println("      ****** BIENVENIDO AL GENERADOR DE CONTRASEÑAS 3000 ******\n "
+				+ "Esta es la lista de opciones que usted puede utilizar para generar su contraseña \n "
+				+ "-A             Genera contraseña con mayusculas, minusculas y numeros excluyendo caracteres ambíguos como (i, I, L, l, 1, o, O, 0)\n"
+				+ " -U             Genera contraseña con letras mayusculas \n"
+				+ " -L             Genera contraseña con letras minuscula \n"
+				+ " -N             Genera contraseña con numeros \n"
+				+ " -S             Genera contraseña con simbolos \n"
+				+ " -T:            Establece la cantidad de contraseñas que desea generar. Ejemplo: -T:3 Genera un total de 3 contraseñas \n"
+				+ " -E:            Excluir símbolos suministrados Ejemplo: -E:$% Genera contraseña sin los caracteres $%  \n"
+				+ "-Size:		Establece la cantidad de caracteres que tendra la contraseña. Ejemplo -Size:8 Genera contraseña con 8 caracteres. \n"
+				+ "-Copy           La contraseña generada queda copiada en el porta pateles, puede utilizar CTRL + V para pegar la contraseña \n "
+				+ "-? 		Muestra el menu de ayuda\n\n"
+				+ "Recuerde puede hacer combinaciones, por ejemplo: Si desea contraseña con números, letras mayúsculas, minúsculas y simbolos puede digitar  \n"
+				+ "lo siguente: -N -U -L -S \n\n"
+				+ "Si no ingresa alguna de estas opciones se genera una contrasesa -T:1 con 16 caracteres -Size:16 con letras mayusculas -U \n"
+				+ "con letras minusculas -L y con numeros -N.\n\n"
+				+ "      *************   GRACIAS POR UTILIZAR NUESTRO SERVICIO   *****************");
+				
+ 
+		
+	}
+}
---
+++ PassGenR/src/PassGenR.java
@@ -0,0 +1,94 @@
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
+
+public class PassGenR {
+
+	public String[] passGenerateR(int quantity, int pass, String generate) {
+		String pswd = "";
+		String[] newPswd = new String[quantity];
+		int numRan = 0;
+		while (quantity > 0) {
+			String[] arraygenerate = generate.split("");
+			for (int i = 0; i < pass; i++) {
+				numRan = ((int) (Math.random() * arraygenerate.length - 1) + 0);
+				pswd = pswd + arraygenerate[numRan];
+			}
+			if (validatePass(pswd)) {
+			}
+			newPswd[quantity - 1] = pswd;
+			pswd = "";
+			quantity--;
+		}
+		return newPswd;
+	}
+
+	private boolean validatePass(String pswd) {
+		if (pswd.matches("^(?=\\w*[0-9])(?=\\w*[A-Z])(?=\\w*[a-z])\\S{1,}$"))
+			return true;
+		return false;
+	}
+
+	public void board(String Pass) {
+
+		StringSelection selection = new StringSelection(Pass);
+		Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+		clipboard.setContents(selection, selection);
+	}
+
+	private static int capture(String values) {
+		String[] joined = values.split(":");
+		return Integer.parseInt(joined[1]);
+	}
+
+	public static void main(String[] args) {
+		PassGenR cr = new PassGenR();
+		HelpMenu ls = new HelpMenu();
+		String U = "ABCDEFGHIJKLNMOPQRSTUVWXYZ";
+		String L = "abcdefghijklmnopqrstuvwxyz";
+		String S = "|@#~$%()=^*+[]{}-_?¿";
+		String N = "1234567890";
+		String A = "ABCDEFGHJKNMPQRSTUVWXYZabcdefghjkmnpqrstuvwxyz23456789";
+		boolean menu = false;
+		int length = 16;
+		int totalPass = 1;
+		boolean itContains = false;
+		String generating = "";
+		for (int i = 0; i < args.length; i++) {
+			if (args[i].indexOf("-Size:") != -1) {
+				length = capture(args[i]);
+			} else if (args[i].indexOf("-T:") != -1) {
+				totalPass = capture(args[i]);
+			} else if (args[i].indexOf("-Copy") != -1) {
+				itContains = true;
+			} else if (args[i].indexOf("-?") != -1) {
+				ls.menu(null);
+				menu = true;
+			} else if (args[i].indexOf("-U") != -1) {
+				generating += U;
+			} else if (args[i].indexOf("-L") != -1) {
+				generating += L;
+			} else if (args[i].indexOf("-N") != -1) {
+				generating += N;
+			} else if (args[i].indexOf("-S") != -1) {
+				generating += S;
+			} else if (args[i].indexOf("-A") != -1) {
+				generating += A;
+			}
+		}
+		if (menu== false ) {
+			if (generating.length() == 0) {
+				generating = N + U + L;
+			}
+			String[] newResultPass = cr.passGenerateR(totalPass, length, generating);
+			for (int i = 0; i < newResultPass.length; i++) {
+				System.out.println(newResultPass[i]);
+				if (itContains == true) {
+					cr.board(newResultPass[i]);
+				
+				}
+			}
+		}
+
+	}
+}
